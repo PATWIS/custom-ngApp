39da724c8e71e95f5300d7eb414b10a69e6d6dd6
PATWIS <patryk.wisniewski@outlook.com>
2017-12-28 23:21:31 +0100

    init babel config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -3,8 +3,22 @@ const {resolve} = require('path');
 module.exports = {
   entry: './src/app.js',
   output: {
-    path: resolve(__dirname, '../dist'),
-    // path: 'C:\\Users\\Patryk\\Infoprojekt Sp. z o.o\\SPDevPWisniewski - testLibrary',
+    // path: resolve(__dirname, '../dist'),
+    path: 'C:\\Users\\Patryk\\Infoprojekt Sp. z o.o\\SPDevPWisniewski - testLibrary',
     filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015']
+          }
+        }
+      }
+    ]
   }
 };
