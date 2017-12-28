576f4e5b9040afd8253812ddf51da2d48ca692d5
PATWIS <patryk.wisniewski@outlook.com>
2017-12-28 23:03:25 +0100

    Added devDependencies Babel / rimraf

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,9 +1,10 @@
-const path = require('path');
+const {resolve} = require('path');
 
 module.exports = {
   entry: './src/app.js',
   output: {
-    path: path.resolve(__dirname, '../dist'),
+    path: resolve(__dirname, '../dist'),
+    // path: 'C:\\Users\\Patryk\\Infoprojekt Sp. z o.o\\SPDevPWisniewski - testLibrary',
     filename: 'app.bundle.js'
   }
 };
